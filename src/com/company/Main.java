4a2688b78922c90470561c93b6400809ddbd74af
jrshort89 <jrshort89@gmail.com>
2021-02-08 11:40:06 -0500

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        boolean myTruth = false;
+
+        System.out.println(myTruth);
+
+        String myString = "this is a string";
+
+        String newString = " new string";
+
+        System.out.println(myString + newString);
+
+        int myInt = 50;
+        String numberString = "50";
+        System.out.println(myInt + numberString);
+
+        int result = 1 + 2;
+        System.out.println(result);
+        result++;
+        result += 2;
+        result *= 2;
+        result /= 2;
+        System.out.println(result);
+    }
+}
